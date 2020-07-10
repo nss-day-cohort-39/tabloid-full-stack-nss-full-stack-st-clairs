530f39813a6b91d63d196a0cbdd0d512644be681
blake slochowsky <blakeslochowsky.com>
2020-07-10 14:03:34 -0500

    edit tags

--- Tabloid/client/src/components/Tag/Tag.js
+++ Tabloid/client/src/components/Tag/Tag.js
@@ -1,28 +1,90 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import { Card, CardBody, Button, Modal, ModalHeader, ModalBody } from "reactstrap";
 import { TagContext } from "../../providers/TagProvider";
 
 export const Tag = ({ tag }) => {
+    const name = useRef()
     
     const [modal, setModal] = useState(false)
     const toggle = () => setModal(!modal)
 
-    const { deleteTag } = useContext(TagContext)
+    const { deleteTag, updateTag } = useContext(TagContext)
+    
+    const [editModal, setEditModal] = useState(false)
+    const toggleEdit = () => setEditModal(!editModal)
+
+    const tagUpdate = () => {
+        updateTag({
+            id: tag.id,
+            name: name.current.value
+        }).then(toggleEdit)
+    }
 
     return (
         <Card className="tagCard">
             <CardBody>
                 <div className="TagCardBody">
                     <h4>{tag.name}</h4>
-                    <div>
-                        <button type="submit"
-                            onClick={
-                                evt => {
-                                    evt.preventDefault()}}
-                            className="btn btn-primary">
-                            Edit</button>
-                        <Button color="danger" onClick={toggle}>Delete</Button>
+                <div>
+                <Button color="primary" onClick={toggleEdit}>Edit</Button>
 
+<Modal isOpen={editModal} toggle={toggleEdit}>
+    <ModalHeader toggle={toggleEdit}>
+        Edit {tag.name}</ModalHeader>
+        <ModalBody >
+        <div className="form-group">
+            <input
+                type="text"
+                id="name"
+                ref={name}
+                required
+                autoFocus
+                className="form-control"
+                defaultValue={tag.name}
+            />
+            <div className="">
+                <button type="submit"
+                    onClick={
+                        evt => {
+                            evt.preventDefault() // Prevent browser from submitting the form
+                            toggleEdit()}}
+                    className="btn btn-secondary">
+                    Cancel</button>
+                <button type="submit"
+                    onClick={
+                        evt => {
+                            evt.preventDefault() // Prevent browser from submitting the form
+                            tagUpdate()}}
+                    className="btn btn-primary">
+                    Save Changes</button>
+            </div>
+        </div>
+    </ModalBody>
+</Modal>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        <Button color="danger" onClick={toggle}>Delete</Button>
                         <Modal isOpen={modal} toggle={toggle}>
                             <ModalHeader toggle={toggle}>
                                Are you sure you want to delete {tag.name}?</ModalHeader>
